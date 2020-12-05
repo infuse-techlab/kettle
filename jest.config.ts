5534567c69cc40fd17e92bed47f271338d7575b2
Julien Andreu <julienandreu@me.com>
2020-12-05 23:05:50 +0100

    Add sonarqube conf

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,24 @@
 export default {
-  coverageDirectory: 'coverage',
-  coveragePathIgnorePatterns: ['/node_modules/'],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/index.ts',
+    '!src/**/*.index.ts',
+    '!src/**/*.mock.ts',
+    '!src/**/*.mock.*.ts',
+  ],
+  coveragePathIgnorePatterns: ['node_modules', 'src/index.ts'],
+  coverageDirectory: 'coverage/',
+  coverageThreshold: {
+    global: {
+      branches: 20,
+      functions: 30,
+      lines: 50,
+      statements: 50,
+    },
+  },
   coverageProvider: 'v8',
+  coverageReporters: ['json-summary', 'text', 'lcov', 'cobertura'],
   testEnvironment: 'node',
   preset: 'ts-jest',
   transform: {
