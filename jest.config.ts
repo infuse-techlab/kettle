3313f0af38ddad804ba054ff2ffe1c6229104e29
Julien Andreu <julienandreu@me.com>
2020-12-07 21:19:37 +0100

    feat: Add Jest aliases

--- jest.config.ts
+++ jest.config.ts
@@ -24,4 +24,11 @@ export default {
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
   },
+  moduleNameMapper: {
+    '^@application/(.*)': '<rootDir>/src/application/$1',
+    '^@core/(.*)': '<rootDir>/src/core/$1',
+    '^@domain/(.*)': '<rootDir>/src/domain/$1',
+    '^@infrastructure/(.*)': '<rootDir>/src/infrastructure/$1',
+    '^@root/(.*)': '<rootDir>/$1',
+  },
 };
